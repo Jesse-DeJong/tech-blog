59f9f7f4214409edc5cf76d41c29044189d45215
Jesse De Jong <jdoceau@gmail.com>
2021-08-29 17:16:45 +1000

    setup handlebars for #each articles

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+const { User, Article } = require('../models');
+
 router.use('/api', apiRoutes);
 
-router.get('/', (req, res) => {
-  res.render('layouts/main')
-});
+// router.get('/', (req, res) => {
+//   res.render('layouts/main')
+// });
 
 //Login route
 router.get('/login', (req, res) => {
@@ -16,6 +18,22 @@ router.get('/login', (req, res) => {
   }
   // Otherwise, render the login template
   res.render('login');
+});
+
+// Homepage articles
+router.get('/', async (req, res) => {
+  // TODO: Render template with Sequelize data
+  const articleData = await Article.findAll({
+    order: [['date_created', 'ASC']]
+  });
+  
+  
+  const articles = articleData.map(articleData => articleData.get({ plain: true }));
+  console.log(articles);
+
+  res.render('homepage', {
+    articles
+  });
 });
 
 module.exports = router;
